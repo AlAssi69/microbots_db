012d58759910a35a5269ac9e2b99245bd892ca0b
Ashraf AlAssi <smart.teen.12345@gmail.com>
2024-04-28 14:00:06 +0300

    start implement commands

---
+++ app/Console/Commands/Import/ImportDataCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console\Commands\Import;
+
+use Illuminate\Console\Command;
+
+class ImportDataCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:data
+    {name : the Excel file containing data}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Imports an Excel file';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
